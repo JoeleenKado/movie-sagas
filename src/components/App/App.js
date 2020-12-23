8ea6817005e395aacc602de6473da5dd17b8ede6
joellekado <jkado@my.hpu.edu>
2020-12-23 12:48:45 -0600

    background colors

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Movies!</h1>
+        <h1>Joel's Movie Gallery</h1>
         <Router>
           {/* ADD PAGES! */}
           <nav>
